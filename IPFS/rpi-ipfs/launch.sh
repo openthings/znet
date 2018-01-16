1e27f6bec8fb132f3bfe99948bf294b4553572e1
OpenThings <openthings@163.com>
2018-01-16 11:01:49 +0800

    create launch for IPFS docker on rpi.

---
+++ IPFS/rpi-ipfs/launch.sh
@@ -0,0 +1,7 @@
+docker run -d --name zrear --restart=always -p 9999:9999 openthings/zrear_rpi
+
+#weave run --name zrear --restart=always -p 9999:9999 -it openthings/zrear
+
+#weave run --name zrear --restart=always -p 8388:8388 -it \
+#	-v $PWD/../config/cow-rear:/root/.cow \
+#	openthings/zrear
